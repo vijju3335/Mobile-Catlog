e1caff74b4fcfc33d97bc13bc755a9011cedba36
Vijju3335 <34649168+vijju3335@users.noreply.github.com>
2018-06-10 09:26:47 +0530

    pep8Online  indented format

--- database_setupMain.py
+++ database_setupMain.py
@@ -6,50 +6,63 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class User(Base):
-    __tablename__ = 'user'
 
+class User(Base):
+    '''
+    This class is to create user table,
+    it takes declarative base object as parameter.
+    '''
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(50), nullable=False)
     email = Column(String(50), nullable=False)
     picture = Column(String(255))
-    
-    # serializable format
+
     @property
     def serialize(self):
+        ''' Return data as object in simple serializeable format'''
         return {
             'name': self.name,
             'id': self.id,
-            'email' : self.email,
-            'picture' : self.picture,
+            'email': self.email,
+            'picture': self.picture,
         }
 
+
 class Brand(Base):
+    '''
+    This class is to create brand table,
+    it takes declarative base object as parameter.
+    '''
     __tablename__ = 'brand'
-    id = Column(Integer, primary_key = True)
+    id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
-    name = Column(String(50),nullable=False)
-   
-    
+    name = Column(String(50), nullable=False)
+
     user = relationship(User)
-    # serializable format
+
     @property
     def serialize(self):
+        ''' Return data as object in simple serializeable format'''
         return {
             'name': self.name,
             'id': self.id,
-            
+
         }
 
 
 class Model(Base):
+    '''
+    This class is to create model table,
+    it takes declarative base object as parameter.
+    '''
     __tablename__ = 'model'
 
-    id = Column(Integer,primary_key = True)
-    brand_id = Column(Integer,ForeignKey('brand.id'))
+    id = Column(Integer, primary_key=True)
+    brand_id = Column(Integer, ForeignKey('brand.id'))
     user_id = Column(Integer, ForeignKey('user.id'))
 
-    name = Column(String(30), nullable = False)
+    name = Column(String(30), nullable=False)
     os = Column(String(30))
     processor = Column(String(30))
     ram = Column(String(30))
@@ -67,27 +80,27 @@ class Model(Base):
     brand = relationship(Brand)
     user = relationship(User)
 
-    # serializable format
     @property
     def serialize(self):
+        ''' Return data as object in simple serializeable format'''
         return {
 
             'id': self.id,
             'name': self.name,
-            'os' : self.os,
-            'processor' : self.processor,
-            'ram' : self.ram,
-            'storage' :  self.storage,
-            'camera' : self.camera,
-            'battery' : self.battery,
-            'connectivity' : self.connectivity,
-            'sim' : self.sim,
-            'colors' : self.colors,
-			'sensors' : self.sensors,
-            'price' : self.price,
-            'description' : self.description,
-            'picture' : self.picture,
-            
+            'os': self.os,
+            'processor': self.processor,
+            'ram': self.ram,
+            'storage':  self.storage,
+            'camera': self.camera,
+            'battery': self.battery,
+            'connectivity': self.connectivity,
+            'sim': self.sim,
+            'colors': self.colors,
+            'sensors': self.sensors,
+            'price': self.price,
+            'description': self.description,
+            'picture': self.picture,
+
         }
 
 engine = create_engine('sqlite:///mobileWorld.db')
